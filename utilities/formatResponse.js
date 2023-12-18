b249f89dbd7a69630d2029a30e3f0ad7c5ce2f48
SamPatt <git@sampatt.com>
2023-12-18 14:08:49 -0500

    Improve formatting to allow valid tags other than div but exlcude react fragments

--- utilities/formatResponse.js
+++ utilities/formatResponse.js
@@ -1,12 +1,14 @@
 function formatValidResponse(aiResponse) {
-    // Find the first opening and last closing div tags
-    const startDiv = aiResponse.indexOf("<div");
-    const endDiv = aiResponse.lastIndexOf("</div>") + 6; // 6 characters to include '</div>'
+    // Regular expression to match the first complete tag (excluding React fragments)
+    const tagPattern = /<([a-z]+[a-z0-9-]*)([^>]*)>([\s\S]*?)<\/\1>/i;
+    const match = aiResponse.match(tagPattern);
 
-    // Extract the valid part of the response
-    const validResponse = startDiv >= 0 && endDiv >= 0 ? aiResponse.slice(startDiv, endDiv) : "";
+    if (match) {
+        // Return the first complete tag with its content
+        return match[0];
+    }
 
-    return validResponse;
+    return ""; // Return empty string if no valid tag is found
 }
 
 module.exports = {
