62f0aca431eac1e069629144a49c785c1408980b
SamPatt <git@sampatt.com>
2023-12-19 14:44:09 -0500

    Update prompt to remove classnames and IDs

--- utilities/openAIRequest.js
+++ utilities/openAIRequest.js
@@ -5,10 +5,10 @@ const { formatValidResponse } = require('./formatResponse.js');
 
 async function aiRequest(conversationHistory, userRequest, baseHtml) {
     const baseRequest = {
-        Task: `You are a coding assistant. Create a new JSX element to replace the element with id 'auto-component-container'. Your returned code will be placed within a React component's return() statement. Follow the guidelines EXTREMELY STRICTLY`,
-        Requirement: `Find the div with id 'auto-component-container' in the HTML Context and provide ONLY the element that fits within its place. This new element should have no classsname.`,
+        Task: `You are a coding assistant. Create a new JSX element to replace the element with classname of 'auto-component'. Your returned code will be placed within a React component's return() statement. Follow the guidelines EXTREMELY STRICTLY`,
+        Requirement: `Find the div with classname of 'auto-component' in the HTML Context and provide ONLY the element that fits within its place. This new element should have no classname.`,
         Guidelines: [
-            `ONLY respond with the CODE you create. Do not provide any additional details or notes about your code, decision making, final product, etc., before OR after the code statement.`,
+            `ONLY respond with the CODE you create. NEVER use classnames or ids. Do not provide any additional details or notes about your code, decision making, final product, etc., before OR after the code statement.`,
             `ONLY respond with code that belongs inside the React fragment.`,
             `Do not include anything other than the necessary elements. This means NO comments, NO opening/closing text explaining the element, etc.`,
             `NEVER include top-level elements like root or App. All elements should fit within the jsx fragments.`,
