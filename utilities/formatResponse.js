0b1c108cec04dfb4fe9f2bd44b42c17a726e2fd3
SamPatt <git@sampatt.com>
2023-12-19 11:41:25 -0500

    Update formatter to remove markdown

--- utilities/formatResponse.js
+++ utilities/formatResponse.js
@@ -1,36 +1,18 @@
 function formatValidResponse(aiResponse) {
-    // Find the position of the first opening tag
-    const firstTagStart = aiResponse.search(/<([a-z]+[a-z0-9-]*)([^>]*)>/i);
+    // Remove markdown code fences if present
+    let cleanedResponse = aiResponse.replace(/```js/g, '').replace(/```/g, '');
 
-    // If no opening tag is found, return an empty string
-    if (firstTagStart === -1) {
-        return "";
+    // Find the first valid opening JSX tag, ignoring React fragments
+    const firstValidTagStart = cleanedResponse.search(/<([a-z]+[a-z0-9-]*)([^>]*)>/i);
+
+    if (firstValidTagStart === -1) {
+        return "Code not valid"; // No valid opening tag found
     }
 
-    // Find the position of the last closing tag by looking for the closing tag of the first opening tag
-    let tagStack = [];
-    let i = firstTagStart;
-    while (i < aiResponse.length) {
-        // Check for an opening tag
-        if (aiResponse[i] === "<" && aiResponse[i+1] !== "/") {
-            let tagEnd = aiResponse.indexOf(">", i);
-            if (tagEnd === -1) break; // No closing '>', invalid HTML
-            tagStack.push(aiResponse.substring(i, tagEnd + 1));
-            i = tagEnd;
-        } else if (aiResponse[i] === "<" && aiResponse[i+1] === "/") { // Closing tag
-            let tagEnd = aiResponse.indexOf(">", i);
-            if (tagEnd === -1) break; // No closing '>', invalid HTML
-            let lastTag = tagStack.pop();
-            // If the stack is empty, we've found the closing tag of the first tag
-            if (tagStack.length === 0) {
-                return aiResponse.substring(firstTagStart, tagEnd + 1);
-            }
-            i = tagEnd;
-        }
-        i++;
-    }
+    // Remove everything before the first valid opening tag
+    cleanedResponse = cleanedResponse.substring(firstValidTagStart);
 
-    return ""; // Return empty string if valid closing tag is not found
+    return cleanedResponse;
 }
 
 module.exports = {
