ee178f727d48fd713d5fe0edb0e5746ad644bbcf
SamPatt <git@sampatt.com>
2023-12-19 17:16:46 -0500

    Sets OPTIONS method response

--- server.js
+++ server.js
@@ -14,11 +14,23 @@ app.use(express.json());
 app.use(morgan("dev")); 
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allows all domains
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    // Respond with 200 status for preflight requests
+    res.sendStatus(200);
+    return;
+  }
+  next();
+});
+
 // app.use("/module", modulesRouter);
 app.use("/api", apiRouter);
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("npm i -D auto-component");
 });
 
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
