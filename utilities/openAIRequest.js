371032746d0c789a6200fe71803356630890a3fb
SamPatt <git@sampatt.com>
2023-12-19 22:55:51 -0500

    Remove HTMLcontext from prompt

--- utilities/openAIRequest.js
+++ utilities/openAIRequest.js
@@ -3,14 +3,13 @@ const OpenAI = require("openai")
 const openai = new OpenAI(process.env.OPENAI_API_KEY);
 const { formatValidResponse } = require('./formatResponse.js');
 
-async function aiRequest(conversationHistory, userRequest, baseHtml) {
+async function aiRequest(conversationHistory, userRequest) {
     const baseRequest = {
-        Task: `You are a coding assistant. Create a new JSX element to replace the element with classname of 'auto-component'. Your returned code will be placed within a React component's return() statement. Follow the guidelines EXTREMELY STRICTLY`,
-        Requirement: `Find the div with classname of 'auto-component' in the HTML Context and provide ONLY the element that fits within its place. This new element should have no classname.`,
+        Task: `You are a coding assistant helping React developers. You are creating JSX code which fulfils the user request and fits inside a React component's return() statement. Follow the guidelines EXTREMELY STRICTLY`,
         Guidelines: [
             `ONLY respond with the CODE you create. NEVER use classnames or ids. Do not provide any additional details or notes about your code, decision making, final product, etc., before OR after the code statement.`,
-            `ONLY respond with code that belongs inside the React fragment.`,
-            `Do not include anything other than the necessary elements. This means NO comments, NO opening/closing text explaining the element, etc.`,
+            `ONLY respond with code that belongs inside the React fragment. NEVER include the fragment itself.`,
+            `Do not include anything other than the necessary elements. This means NO comments, NO opening/closing text explaining the element, NO markdown, etc.`,
             `NEVER include top-level elements like root or App. All elements should fit within the jsx fragments.`,
             `Ensure seamless integration into the existing code and only use react-based styling, i.e., style={{display:'flex'}}.`,
             `If using colors, guarantee visibility of font colors with selected background colors.`,
@@ -21,12 +20,12 @@ async function aiRequest(conversationHistory, userRequest, baseHtml) {
 
     // Add base request as a system message only if it's a new conversation
     if (conversationHistory.length === 0) {
-        let systemMessageContent = `Task: ${baseRequest.Task}\nRequirement: ${baseRequest.Requirement}\nGuidelines: ${baseRequest.Guidelines.join('\n')}`;
+        let systemMessageContent = `Task: ${baseRequest.Task}\nGuidelines: ${baseRequest.Guidelines.join('\n')}`;
         conversationHistory.push({ role: "system", content: systemMessageContent });
     }
 
-    // Construct the user prompt including userRequest and baseHtml
-    let userPrompt = `User Request: ${userRequest}\nHTML Context: ${baseHtml}`;
+    // Construct the user prompt including userRequest and 
+    let userPrompt = `User Request: ${userRequest}`;
     
     // Append the new user request to the conversation history
     conversationHistory.push({ role: "user", content: userPrompt });
